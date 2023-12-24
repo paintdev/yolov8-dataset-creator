8d01d4004d4ccc6755cc02392eb814f0c17ee024
paintdev <the.paint.developer@gmail.com>
2023-12-24 20:24:01 +0200

    image can be seen and be selected

--- draw.gd
+++ draw.gd
@@ -11,6 +11,11 @@ func  _draw() -> void:
 	var actual = Rect2(start_point,actual_end_point-start_point)
 	draw_rect(actual,Color.RED,false)
 
+	draw_circle(Vector2(start_point.x + ((actual_end_point.x - start_point.x) / 2),
+			start_point.y + ((actual_end_point.y-start_point.y) / 2)),2.5,Color.WHITE)
+
+	get_viewport().size = $Sprite2D.texture.get_size()
+
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("left_click"):
 		start_point = get_local_mouse_position()
