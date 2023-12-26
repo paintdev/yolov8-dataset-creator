c443c70c2060c54651757197f6a6aac6b4b5ba7e
paintdev <the.paint.developer@gmail.com>
2023-12-26 11:33:36 +0200

    added class spesification

--- draw.gd
+++ draw.gd
@@ -1,11 +1,14 @@
 extends Node2D
 
+@onready var selecting = $"Uİ/TopLeft"
+
 var start_point: Vector2
 var end_point: Vector2
 var actual_end_point: Vector2
 var drawn := true
 var drawn_list := []
-
+var Class := 0
+var Class_list := ['Tasit','İnsan','UAP','UAİ']
 
 func  _draw() -> void:
 	var picture_size = $Sprite2D.texture.get_size()
@@ -22,7 +25,6 @@ func  _draw() -> void:
 		var center = Vector2(start_point.x + ((actual_end_point.x - start_point.x) / 2),
 			start_point.y + ((actual_end_point.y-start_point.y) / 2))
 
-		var Class = 0
 		var x_center = center.x/picture_size.x
 		var y_center = center.y/picture_size.y
 		var width = (actual_end_point.x - start_point.x) / picture_size.x
@@ -32,9 +34,11 @@ func  _draw() -> void:
 
 		draw_circle(center,2.5,Color.WHITE)
 
-	get_viewport().size = $Sprite2D.texture.get_size()
+	var image_size = $Sprite2D.texture.get_size()
+	get_viewport().size = image_size
+	$"Uİ".size = image_size
 
-func _input(event: InputEvent) -> void:
+func _input(_event: InputEvent) -> void:
 	#print(get_local_mouse_position())
 
 	if Input.is_action_just_pressed("left_click"):
@@ -50,5 +54,16 @@ func _input(event: InputEvent) -> void:
 	if not drawn:
 		queue_redraw()
 
-
+	if Input.is_key_pressed(KEY_0):
+		selecting.text = 'Tasit'
+		Class = 0
+	if Input.is_key_pressed(KEY_1):
+		selecting.text = 'İnsan'
+		Class = 1
+	if Input.is_key_pressed(KEY_2):
+		selecting.text = 'UAP'
+		Class = 2
+	if Input.is_key_pressed(KEY_3):
+		selecting.text = 'UAİ'
+		Class = 3
 
