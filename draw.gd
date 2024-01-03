607b7a7ff353a35d1ebf4322f5fa8a8f3aa34adc
paintdev <the.paint.developer@gmail.com>
2024-01-03 16:52:38 +0200

    hotfix

--- draw.gd
+++ draw.gd
@@ -36,7 +36,7 @@ func  _draw() -> void:
 		var width = (actual_end_point.x - start_point.x) / image_size.x
 		var height = (actual_end_point.y - start_point.y) / image_size.y
 
-		object_list.append(' '.join([str(Class),x_center,y_center,width,height]))
+		object_list.append(' '.join([str(Class),x_center,y_center,abs(width),abs(height)]))
 		prints(object_list)
 
 		draw_circle(center,2.5,Color.WHITE)
@@ -69,8 +69,8 @@ func _input(_event: InputEvent) -> void:
 		queue_redraw()
 
 	if Input.is_key_pressed(KEY_1):
-		selecting[0].text = 'Tasit'
-		selecting[1].text = 'Tasit'
+		selecting[0].text = 'Taşıt'
+		selecting[1].text = 'Taşıt'
 		Class = 0
 	if Input.is_key_pressed(KEY_2):
 		selecting[0].text = 'İnsan'
@@ -84,7 +84,7 @@ func _input(_event: InputEvent) -> void:
 		selecting[0].text = 'UAİ'
 		selecting[1].text = 'UAİ'
 		Class = 3
-	if Input.is_key_pressed(KEY_N):
+	if Input.is_physical_key_pressed(KEY_N):
 		Nextİmage()
 		ChangeWindowSize(image.texture.get_size())
 
@@ -92,6 +92,7 @@ func _input(_event: InputEvent) -> void:
 		Reset()
 
 func _on_file_dialog_dir_selected(dir: String) -> void:
+	$Label.visible = false
 	directory = dir
 	inMenu = false
 	Nextİmage()
@@ -106,37 +107,46 @@ func ChangeWindowSize(image_size):
 func Nextİmage():
 	files = DirAccess.get_files_at(directory+"/images/")
 
-	SaveData()
-	drawn_list = []
-	start_point = Vector2.ZERO
-	end_point = Vector2.ZERO
-	actual_end_point = Vector2.ZERO
-	queue_redraw()
+	if drawn_list != []:
+		SaveData()
 
 	if index <= (len(files) - 1):
 		if FileAccess.file_exists(directory+"/images/"+files[index]):
-			print(directory+"/images/"+files[index])
 			file_name = files[index]
+			print(directory+"/images/"+files[index])
 			print(file_name)
 			var next_image = Image.load_from_file(directory+"/images/"+file_name)
 			var texture = ImageTexture.create_from_image(next_image)
 			image.texture = texture
 			image.visible = true
-			print(files[index])
-			index += 1
+
 	else:
 		$"Uİ/NO MORE".visible = true
+		$Sprite2D.visible = false
+		await get_tree().create_timer(5).timeout
+		get_tree().quit()
+
+
 
 func SaveData():
 	var data = ""
 	if index <= (len(files) - 1):
 		if file_name:
 			file_name = files[index].split(".")[0]
+			prints(file_name,"is saved")
 			var file = FileAccess.open(directory+"/labels/"+file_name+".txt",FileAccess.WRITE)
 			for i in object_list:
 				data += (i + "\n")
 			file.store_string(data)
 			object_list = []
+			index += 1
+
+	drawn_list = []
+	start_point = Vector2.ZERO
+	end_point = Vector2.ZERO
+	actual_end_point = Vector2.ZERO
+	queue_redraw()
+
 
 func Reset():
 	drawn_list = []
